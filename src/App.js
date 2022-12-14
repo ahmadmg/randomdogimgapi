22c8df3b61de4ce2a8cf27b37e76df896ed45132
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-12-14 13:57:41 +0100

    File Size

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 
 function App() {
   let [dogImage, setDogImage] = useState(null);
-
+  let [fileSize, setfileSize] = useState(0);
   useEffect(() => {
     fetch("https://random.dog/woof.json")
       .then((response) => response.json())
-      .then((result) => setDogImage(result.url));
+      .then((result) => {
+        setDogImage(result.url);
+        setfileSize(result.fileSizeBytes);
+      });
   }, []);
   return (
     <div>
-      <img src={dogImage} />
+      <img src={dogImage} className="dog-image" />
+      <h2>The File Size is : {fileSize} Bytes </h2>
     </div>
   );
 }
